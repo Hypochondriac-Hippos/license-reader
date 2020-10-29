ee8e909cfd0bba2ce32c22758d8598687f81beaf
wecassidy <wec.notif.github@gmail.com>
2020-10-29 15:52:43 -0400

    Use SIFT to try to ID license plates

--- enph353/enph353_gazebo/nodes/license_detector.py
+++ enph353/enph353_gazebo/nodes/license_detector.py
@@ -16,6 +16,7 @@ import os
 import cv_bridge
 import cv2
 import rospy
+import rospkg
 from sensor_msgs import msg
 
 import util
@@ -26,17 +27,58 @@ TOPICS = {
     "debug": "/hippo/license_debug",
 }
 
-bridge = cv_bridge.CvBridge()
-debug_stream = rospy.Publisher(TOPICS["debug"], msg.Image, queue_size=1)
-
 DEBUG = os.getenv("HIPPO_DEBUG", 0) != 0
+if DEBUG:
+    debug_stream = rospy.Publisher(TOPICS["debug"], msg.Image, queue_size=1)
 
-plates = {n: util.imread("plates/p{}.png".format(n)) for n in "12345678"}
+bridge = cv_bridge.CvBridge()
+rospack = rospkg.RosPack()
+plates = {
+    n: util.imread(
+        os.path.join(
+            rospack.get_path("enph353_gazebo"), "media", "plates", "p{}.png"
+        ).format(n)
+    )
+    for n in "12345678"
+}
+sift = cv2.xfeatures2d.SIFT_create()
+flann = cv2.FlannBasedMatcher({"algorithm": 1, "trees": 5}, {"checks": 50})
+plate_kp = {
+    n: sift.detectAndCompute(image, mask=None) for n, image in plates.iteritems()
+}
+
+MIN_MATCH_COUNT = 10
 
 
 def detect_plate(ros_image):
     image = bridge.imgmsg_to_cv2(ros_image, "bgr8")
-    debug_stream.publish(bridge.cv2_to_imgmsg(image, "bgr8"))
+    keypoints, descriptors = sift.detectAndCompute(image, mask=None)
+    potential_plates = ""
+    for plate_n, (_, plate_desc) in plate_kp.iteritems():
+        matches = flann.knnMatch(descriptors, plate_desc, k=2)
+        matchesMask = [[0, 0] for i in range(len(matches))]
+
+        # Ratio test
+        good = []
+        for i, (m, n) in enumerate(matches):
+            if m.distance / n.distance < 0.7:
+                matchesMask[i] = [1, 0]
+                good.append(m)
+        if len(good) > MIN_MATCH_COUNT:
+            potential_plates += plate_n
+
+    if DEBUG:
+        if potential_plates == "":
+            potential_plates = "None"
+        image = cv2.putText(
+            image,
+            potential_plates,
+            (0, 50),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (255, 255, 255),
+        )
+        debug_stream.publish(bridge.cv2_to_imgmsg(image, "bgr8"))
 
 
 if __name__ == "__main__":
